10607bc412e0f6ad367e4a0a3b71bd3b302ef3f7
Akib Hossain <akib.hossain737@gmail.com>
2023-08-09 13:42:22 -0700

    ADDED: items updates

--- src/Home.js
+++ src/Home.js
@@ -2,40 +2,36 @@
 import React from 'react';
 import './App.css';
 import styled from 'styled-components';
-import { FaHome } from 'react-icons/fa';
-import { FaGithub } from 'react-icons/fa';
-import { FaTwitter } from 'react-icons/fa';
-import { FaInstagram } from 'react-icons/fa';
-import { FaLinkedin } from 'react-icons/fa';
+import { FaHome, FaGithub, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa';
 const Home = () => {
-  return (
-    <>
-        <Container>
-            <Main>
-                <Profile>
-                    <Img src='/images/profile1.jpg' />
-                    <UserTitle>@vanillacraftz</UserTitle>
-                </Profile>
-                <Menu>
-                    <MenuTitle><a href='https://www.instagram.com/vanillacode737/' ><FaHome className='iconStyle'/>official website</a></MenuTitle>
-                    <MenuTitle><a href='https://github.com/vanillacode737' ><FaGithub className='iconStyle'/>Github</a></MenuTitle>
-                    <MenuTitle><a href='https://twitter.com/vanillacode737' ><FaTwitter className='iconStyle'/>Twitter</a></MenuTitle>
-                    <MenuTitle><a href='https://www.instagram.com/vanillacode737/' ><FaInstagram className='iconStyle'/>Instagram</a></MenuTitle>
-                    <MenuTitle><a href='https://www.linkedin.com/in/vanillacode737/' ><FaLinkedin className='iconStyle'/>Linkedin</a></MenuTitle>
-                </Menu>
-            </Main>
-        </Container>
-    </>
-  )
+    return (
+        <>
+            <Container>
+                <Main>
+                    <Profile>
+                        <Img src='/images/profile1.jpg' />
+                        <UserTitle>@vanillacraftzz</UserTitle>
+                    </Profile>
+                    <Menu>
+                        <MenuTitle><a href='https://www.instagram.com/vanillacraftzz/' ><FaHome className='iconStyle' />official website</a></MenuTitle>
+                        <MenuTitle><a href='https://github.com/vanillacraftzz' ><FaGithub className='iconStyle' />Github</a></MenuTitle>
+                        <MenuTitle><a href='https://twitter.com/vanillacraftzz' ><FaTwitter className='iconStyle' />Twitter</a></MenuTitle>
+                        <MenuTitle><a href='https://www.instagram.com/vanillacraftzz/' ><FaInstagram className='iconStyle' />Instagram</a></MenuTitle>
+                        <MenuTitle><a href='https://www.linkedin.com/in/vanillacraftzz/' ><FaLinkedin className='iconStyle' />Linkedin</a></MenuTitle>
+                    </Menu>
+                </Main>
+            </Container>
+        </>
+    )
 }
 
 export default Home;
 
 const Container = styled.div`
     display: flex;
+    position: relative;
     justify-content: center;
     align-items: center;
-    position: relative;
     height: 100vh;
     width: 100%;
     background-image: url('/images/bg.jpg');
@@ -57,6 +53,7 @@ const Container = styled.div`
 `
 
 const Main = styled.div``
+
 const Profile = styled.div`
     display: flex;
     margin-bottom: 30px;
@@ -65,31 +62,31 @@ const Profile = styled.div`
     flex-direction: column;
 `
 const Img = styled.img`
-    width: 100px;
-    height: 100px;
+    margin-bottom: 10px;
     border-radius: 50%;
     border: 1px solid rgb(52, 121, 224);
     object-fit: cover;
-    margin-bottom: 10px;
+    width: 100px;
+    height: 100px;
 `
 const UserTitle = styled.p`
-display: inline-block;
-padding: 5px 6px;
-border: 1px solid #fff;
-border-radius: 20px;
-background-color: #000;
-color: #fff;
+    display: inline-block;
+    padding: 5px 6px;
+    border: 1px solid #fff;
+    border-radius: 20px;
+    background-color: #000;
+    color: #fff;
 `
 
 const Menu = styled.div``
 const MenuTitle = styled.div`
-    text-align: center;
     margin-bottom: 20px;
-    background-color: #fff;
-    border-radius: 40px;
     padding: 20px 100px;
+    border-radius: 40px;
     font-size: 22px;
     text-transform: capitalize;
+    text-align: center;
+    background-color: #fff;
     @media(max-width: 768px) {
         padding: 8px 30px;
         font-size: 17px;
