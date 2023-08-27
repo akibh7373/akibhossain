06e3a9406c0d655d07c517e6bfe15b84c609168d
Akib Hossain <44940208+vanillacraftzz@users.noreply.github.com>
2023-08-27 10:18:29 -0700

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
                 <Main>
                     <Profile>
                         <Img src='/images/profile.jpeg' />
-                        <UserTitle>@vanillacraftzz</UserTitle>
+                        <UserTitle>@akibhossin</UserTitle>
                     </Profile>
                     <Menu>
                         <MenuTitle><a href='https://www.instagram.com/vanillacraftzz/' ><FaHome className='iconStyle' />official website</a></MenuTitle>
