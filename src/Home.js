614384c36f522edb926e1b112a1ed740f31fc353
Akib Hossain <44940208+akibh7373@users.noreply.github.com>
2023-09-18 09:35:06 -0700

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -14,10 +14,10 @@ const Home = () => {
                     </Profile>
                     <Menu>
                         {/* <MenuTitle><a href='https://www.instagram.com/vanillacraftzz/' ><FaHome className='iconStyle' />official website</a></MenuTitle> */}
-                        <MenuTitle><a href='https://github.com/vanillacraftzz' ><FaGithub className='iconStyle' />Github</a></MenuTitle>
+                        <MenuTitle><a href='https://github.com/akibh7373' ><FaGithub className='iconStyle' />Github</a></MenuTitle>
                         <MenuTitle><a href='https://twitter.com/vanillacraftzz' ><FaTwitter className='iconStyle' />Twitter</a></MenuTitle>
-                        <MenuTitle><a href='https://www.instagram.com/vanillacraftzz/' ><FaInstagram className='iconStyle' />Instagram</a></MenuTitle>
-                        <MenuTitle><a href='https://www.linkedin.com/in/akibhossin/' ><FaLinkedin className='iconStyle' />Linkedin</a></MenuTitle>
+                        <MenuTitle><a href='https://www.instagram.com/akibh7373/' ><FaInstagram className='iconStyle' />Instagram</a></MenuTitle>
+                        <MenuTitle><a href='https://www.linkedin.com/in/akibh7373/' ><FaLinkedin className='iconStyle' />Linkedin</a></MenuTitle>
                     </Menu>
                 </Main>
             </Container>
